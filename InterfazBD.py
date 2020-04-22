599f791f36de91a1f909e0163402a9de9a2ee9eb
ederivc <61289461+ederivc@users.noreply.github.com>
2020-04-22 11:22:56 -0500

    Add files via upload

--- InterfazBD.py
+++ InterfazBD.py
@@ -26,20 +26,46 @@ LabelTit.pack_propagate(0)
 LabelTit.pack(side='top', padx=0, pady=0, anchor='w')
 LabelTit.config(font=("Courier", 20))
 
+#-----------------------------------------------------------------------
 LabelAgregar = tk.Label(frameIzq, width=100, height=1, text = 'AGREGAR', bg = 'purple')
 LabelAgregar.pack_propagate(0)
 LabelAgregar.pack(side='top', padx=10, pady=5, anchor='center')
+#---------------------------------------------------------------------------
 
-#TREE 1
-data_tree = ttk.Treeview(frameIzq, height = 11)
-data_tree.pack_configure(padx=10, pady=10)
+#FRAMES Y LABELS DE AGREGAR
+frameAg = tk.Frame(frameIzq, width=200, height=240, bg = 'blue')
+frameAg.pack_propagate(0)
+frameAg.pack(side='top', padx=10, pady=20, anchor='center')
 
+LabelEmpleado = tk.Label(frameAg, width=100, height=1, text = 'Empleado', bg = 'purple')
+LabelEmpleado.pack_propagate(0)
+LabelEmpleado.pack(side='top', padx=10, pady=10, anchor='w')
+
+LabelProveedor = tk.Label(frameAg, width=100, height=1, text = 'Proveedor', bg = 'purple')
+LabelProveedor.pack_propagate(0)
+LabelProveedor.pack(side='top', padx=10, pady=10, anchor='w')
+
+LabelProducto = tk.Label(frameAg, width=100, height=1, text = 'Producto', bg = 'purple')
+LabelProducto.pack_propagate(0)
+LabelProducto.pack(side='top', padx=10, pady=10, anchor='w')
+
+LabelCategoria = tk.Label(frameAg, width=100, height=1, text = 'Categoria', bg = 'purple')
+LabelCategoria.pack_propagate(0)
+LabelCategoria.pack(side='top', padx=10, pady=10, anchor='w')
+
+#----------------------------------------------------------------------------
 LabelMostrar = tk.Label(frameIzq, width=100, height=1, text = 'MOSTRAR', bg = 'purple')
 LabelMostrar.pack_propagate(0)
 LabelMostrar.pack(side='top', padx=10, pady=3, anchor='w')
+#----------------------------------------------------------------------------
 
-#TREE 2
-data_tree2 = ttk.Treeview(frameIzq, height = 11)
-data_tree2.pack_configure(padx=10, pady=10)
+#FRAMES Y LABELS DE MOSTRAR
+frameMo = tk.Frame(frameIzq, width=200, height=240, bg = 'yellow')
+frameMo.pack_propagate(0)
+frameMo.pack(side='top', padx=10, pady=20, anchor='center')
+
+LabelTabla = tk.Label(frameMo, width=100, height=1, text = 'MOSTRAR', bg = 'purple')
+LabelTabla.pack_propagate(0)
+LabelTabla.pack(side='top', padx=10, pady=3, anchor='w')
 
 main()
